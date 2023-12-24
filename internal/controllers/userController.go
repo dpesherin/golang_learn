a5c9fa6a51d3e70270d17e271db1bbff78ad1a28
Denis <d.pesherin@ctrl.lc>
2023-12-24 13:56:13 +0300

    add plate to authRouter with 200 HTTP OK

--- internal/controllers/userController.go
+++ internal/controllers/userController.go
@@ -1,2 +1,10 @@
 package controllers
 
+import "github.com/gin-gonic/gin"
+
+func RegController(c *gin.Context) {
+	c.Status(200)
+}
+func LoginController(c *gin.Context) {
+	c.Status(200)
+}
--- internal/routers/authRouter.go
+++ internal/routers/authRouter.go
@@ -1,14 +1,14 @@
 package router
 
 import (
-	"partner.portal/internal/handlers"
+	"partner.portal/internal/controllers"
 	"partner.portal/internal/httpServer"
 )
 
 func RunAuthRouters() {
 	g := httpServer.GetInstance().Group("/api/v1/auth")
 	{
-		g.POST("/register", handlers.RegHandler)
-		g.POST("/login", handlers.LoginHandler)
+		g.POST("/register", controllers.RegController)
+		g.POST("/login", controllers.LoginController)
 	}
 }
